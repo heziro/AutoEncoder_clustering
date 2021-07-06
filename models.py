ae9057434d67c0a5750a725f919e61f28e999101
Hezi <heziro1@gmail.com>
2021-07-06 19:34:46 +0300

    init with kmeans

--- models.py
+++ models.py
@@ -8,31 +8,38 @@ import torch.optim as optim
 # https://github.com/XifengGuo/DCEC/blob/master/DCEC.py - TF code
 
 class clusteringLayer(nn.Module):
-    def __init__(self, n_clusters, in_features, alpha=1.0) -> None:
+    def __init__(self, n_clusters, dim, alpha=1.0) -> None:
         super(clusteringLayer, self).__init__()
 
-        self.in_features = in_features
+        self.dim = dim
         self.n_clusters = n_clusters
         self.alpha = alpha
-        self.mu = nn.Parameter(torch.Tensor(
-            self.n_clusters, self.in_features))
+        self.mu = nn.Parameter(torch.Tensor(self.n_clusters, self.dim))
         self.mu = nn.init.xavier_uniform_(self.mu)
         pass
 
     def forward(self, x):
-        q1 = ((1.0 + torch.abs(x.unsqueeze(1) - torch.t(self.mu))**2) /
-              self.alpha)**((-1*self.alpha + 1.0)/2.0)
-        q2 = torch.sum(((1.0 + torch.abs(x.unsqueeze(1) - torch.t(self.mu))**2)/self.alpha)
-                       ** ((-1*self.alpha + 1.0)/2.0), dim=1)
-        q = torch.sum(q1, dim=2) / torch.sum(q2, dim=1).view(-1, 1)
+        q1 = 1.0 / (1.0 + torch.norm(x.unsqueeze(1) - self.mu, p=2, dim=-1)**2)
+        q2 = 1.0 / torch.sum((1.0 + torch.norm(x.unsqueeze(1) - self.mu, p=2, dim=-1)**2), dim=-1)
+        q = q1 / q2.unsqueeze(1)
+
+        # q1 = ((1.0 + torch.abs(x.unsqueeze(1) - torch.t(self.mu)) ** 2) /
+        #       self.alpha) ** ((-1 * self.alpha + 1.0) / 2.0)
+        # q2 = torch.sum(((1.0 + torch.abs(x.unsqueeze(1) - torch.t(self.mu)) ** 2) / self.alpha)
+        #                ** ((-1 * self.alpha + 1.0) / 2.0), dim=1)
+        # q = torch.sum(q1, dim=2) / torch.sum(q2, dim=1).view(-1, 1)
         return q
+
+    def set_mu(self, tensor):
+        self.mu = nn.Parameter(tensor)
 
 
 class ClusterAutoEncoder(nn.Module):
-    def __init__(self, input_shape, n_clusters) -> None:
+    def __init__(self, input_shape, n_clusters, dim) -> None:
         super(ClusterAutoEncoder, self).__init__()
 
         self.input_shape = input_shape
+        self.dim = dim
         self.n_clusters = n_clusters
         self.encoder = nn.Sequential(
             nn.Conv2d(1, 16, 3, stride=3, padding=1),
@@ -43,14 +50,11 @@ class ClusterAutoEncoder(nn.Module):
             nn.MaxPool2d(2, stride=1)
         )
 
-        # self.in_features = self.input_shape[0] // 3 // 2 * \
-        #     self.input_shape[1] // 3 // 2
-        self.in_features = 2*8*2
-        self.embedding = nn.Linear(self.in_features, self.n_clusters)
-        self.deembedding = nn.Linear(self.n_clusters, self.in_features)
+        self.embedding = nn.Linear(8*4, self.dim)
+        self.deembedding = nn.Linear(self.dim, 8*4)
 
         self.cluster_layer = clusteringLayer(
-            self.n_clusters, in_features=self.n_clusters+1)
+            self.n_clusters, dim=self.dim)
 
         self.decoder = nn.Sequential(
             nn.ConvTranspose2d(8, 16, 3, stride=2),
@@ -61,12 +65,20 @@ class ClusterAutoEncoder(nn.Module):
             nn.Tanh()
         )
 
+    def calc_p(self, q):
+        p1 = q*q / (torch.sum(q, dim=0))
+        p2 = torch.sum((q*q / (torch.sum(q, dim=0))), dim=1)
+        return p1 / p2.unsqueeze(1)
+
+
     def forward(self, x):
         x = self.encoder(x)
         x = torch.flatten(x, start_dim=1)
-        x = self.embedding(x)
-        q = self.cluster_layer(x)
-        x = self.deembedding(x)
+        latent = self.embedding(x)
+        q = self.cluster_layer(latent)
+        x = self.deembedding(latent)
         x = x.view(-1, 8, 2, 2)
         x = self.decoder(x)
-        return x, q
+        # clac p
+        p = self.calc_p(q)
+        return x, q, latent, p
--- train.py
+++ train.py
@@ -8,6 +8,7 @@ import torch.nn.functional as F
 import models
 import utils as u
 import torch.optim as optim
+from sklearn.cluster import KMeans
 
 
 def load_data(batch_size=16, num_workers=2):
@@ -26,48 +27,98 @@ def load_data(batch_size=16, num_workers=2):
     return trainloader, testloader
 
 
-def train():
-    batch_size = 32
-    trainloader, testloader = load_data(batch_size=batch_size)
-    # get some random training images
-    dataiter = iter(trainloader)
-    images, labels = dataiter.next()
+def pretrained(model, train_loader, test_loader, rec_criterion, optimizer, batch_size, epochs):
 
-    # show images
-    u.imshow(torchvision.utils.make_grid(images))
-
-    model = models.ClusterAutoEncoder(input_shape=(32, 32, 1), n_clusters=10)
-
-    criterion = nn.MSELoss()
-    optimizer = optim.Adam(model.parameters(), lr=0.001)
-
-    total_loss = 0
-    for epoch in range(10):
-
-        # running_loss = 0.0
-        for i, data in enumerate(trainloader, 0):
+    for epoch in range(epochs):
+        for i, data in enumerate(train_loader, 0):
             inputs, labels = data
 
             optimizer.zero_grad()
-
-            outputs = model(inputs)
-            loss = criterion(outputs[0], inputs)
-            # loss = criterion(outputs[0], inputs)
+            x, q, latent, p = model(inputs)
+            # show reconsruction of x
+            # plt.imshow(x[0, 0].data.numpy());
+            # plt.show()
+            #
+            loss = rec_criterion(x, inputs)
+            # total_loss = (1.0 - gamma) * rec_loss + gamma * clustering_loss * 0.01
             loss.backward()
             optimizer.step()
 
-            total_loss += loss.data
-
-            # running_loss += loss.item()
-
-        print('epoch: [%d] loss: %.3f total loss: %.3f' %
-              (epoch + 1, loss, total_loss))
-
-    testiter = iter(testloader)
-    images, labels = testiter.next()
-    outputs = model(images)
     return model
 
 
+def init_mu(model, train_loader, device):
+    features = None
+    for i, data in enumerate(train_loader, 0):
+        inputs, labels = data
+        model.eval()
+        x, q, latent, p = model(inputs)
+        if features is None:
+            features = latent.cpu().detach().numpy()
+        else:
+            features = np.concatenate((features, latent.cpu().detach().numpy()), 0)
+    km = KMeans(n_clusters=model.n_clusters, n_init=20)
+    km.fit_predict(features)
+    mu = torch.from_numpy(km.cluster_centers_)
+    model.cluster_layer.set_mu(mu.to(device))
+
+    model.train()
+    return model
+
+
+
+
+def train(model, train_loader, test_loader, rec_criterion, cluster_criterion, optimizer, batch_size, epochs, gamma, pretrained_epochs):
+
+    model = pretrained(model, train_loader, test_loader, rec_criterion, optimizer, batch_size, pretrained_epochs)
+    # init mu with kmeans
+    model = init_mu(model, train_loader, 'cpu')
+    total_loss = 0
+    for epoch in range(epochs):
+        for i, data in enumerate(train_loader, 0):
+            inputs, labels = data
+
+            optimizer.zero_grad()
+            x, q, latent, p = model(inputs)
+            # show reconsruction of x
+            # plt.imshow(x[0, 0].data.numpy());
+            # plt.show()
+            #
+            rec_loss = rec_criterion(x, inputs)
+            clustering_loss = kl_loss(p, q)
+            total_loss = (1.0-gamma)*rec_loss + gamma*clustering_loss *0.01
+            total_loss.backward()
+            optimizer.step()
+
+
+        print('epoch: [%d] rec_loss: %.3f, clustering_loss: %.3f, total loss: %.3f' %
+              (epoch + 1, rec_loss, clustering_loss, total_loss))
+
+    test_iter = iter(test_loader)
+    images, labels = test_iter.next()
+    outputs = model(images)
+    pass
+
+def kl_loss(p, q):
+    return -1.0 * torch.sum(torch.sum(p*torch.log(p/q), dim=1), dim=0)
+
 if __name__ == "__main__":
-    model = train()
+    model = models.ClusterAutoEncoder(input_shape=(32, 32, 1), n_clusters=10, dim=11)
+    optimizer = optim.Adam(model.parameters(), lr=0.001)
+    rec_criterion = nn.MSELoss()
+    cluster_criterion = nn.MSELoss()
+    batch_size = 32
+    epochs = 10
+    pretrained_epochs = 1
+    gamma = 0.5
+    train_loader, test_loader = load_data(batch_size=batch_size)
+
+    # get some random training images
+    data_iter = iter(train_loader)
+    images, labels = data_iter.next()
+
+    # show images
+    # u.imshow(torchvision.utils.make_grid(images))
+
+    train(model, train_loader, test_loader, rec_criterion, cluster_criterion, optimizer, batch_size, epochs, gamma,
+          pretrained_epochs=pretrained_epochs)
